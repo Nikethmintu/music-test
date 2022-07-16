20c1b1089359947d91f8711eb062134dd9e62674
Nikethmintu <89294186+Nikethmintu@users.noreply.github.com>
2022-07-16 11:23:15 +0530

    Update ping.js

--- commands/slash/misc/ping.js
+++ commands/slash/misc/ping.js
@@ -1,22 +1,58 @@
+const {
+	MessageEmbed,
+	MessageActionRow,
+	MessageButton,
+	Permissions,
+} = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed: Embed } = require("discord.js");
-const client = new Discord.Client();
+
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("ping")
-		.setDescription("bot latency"),
+		.setName("pinb")
+		.setDescription("get ping of bot"),
+
 	async execute(interaction) {
 		const commands = interaction.client.slashCommands;
 		const client = interaction.client;
 
-        const ping = new Discord.MessageEmbed()
-        .setTitle(':ping_pong: Pong!')
-		.addField("Bot ping:", + `${Math.floor(msg.createdTimestamp - message.createdTimestamp)}` + "ms")
-		.addField("Api ping:", + `${Math.round(client.ws.ping)}` + "ms")
-        .setColor('RANDOM')
-		.setTimestamp()
-        msg.edit(ping);
-        
-    })
-}
-}
+		const ping = new MessageEmbed()
+			.setColor(`RED`)
+			.setAuthor(
+				`${interaction.user.username}`,
+				`${interaction.user.avatarURL({ dynamic: true })}`,
+				`https://discord.com/users/${interaction.user.id}`
+			)
+			.setDescription("Bot ping:", + `${Math.floor(msg.createdTimestamp - message.createdTimestamp)}` + "ms")
+			.setTitle(`**${client.user.username}**`)
+			.setThumbnail(client.user.avatarURL({ dynamic: true }))
+			.setFooter(
+				`${client.user.username.toUpperCase()} ${new Date().getFullYear()}`,
+				client.user.avatarURL({ dynamic: true })
+			)
+			.setTimestamp();
+
+		);
+		const invite = client.generateInvite({
+			permissions: [Permissions.FLAGS.ADMINISTRATOR],
+			scopes: ["bot", "applications.commands"],
+		});
+		const kool = new MessageActionRow().addComponents(
+			new MessageButton()
+				.setLabel("Invite")
+				.setStyle("LINK")
+				.setURL(invite)
+				.setEmoji("🔗"),
+			new MessageButton()
+				.setLabel("support")
+				.setStyle("LINK")
+				.setURL("https://discord.gg/fjqR3YxyZf")
+				.setEmoji("744345792172654643"),
+
+		);
+
+		await interaction.editReply({
+			embeds: [ping],
+			components: [kool],
+		});
+	},
+};
